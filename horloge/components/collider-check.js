7100a33c6f3cda4a27168d0fdf28c81d16843137
NikoStage <niko>
2018-01-16 16:57:12 -0500

    Changement du nom du fichier et commencement du raycasting pour obtenir l'heure.

---
+++ horloge/components/collider-check.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent('collider-check', {
+  dependencies: ['raycaster'],
+
+
+  init: function () {
+    var monHorloge = document.querySelector("#horloge");
+    var monAiguille = document.querySelector("#monAiguille");
+
+    this.el.addEventListener('raycaster-intersected-cleared', function () {
+
+      console.log(this.getAttribute("text").value);
+
+    });
+
+    monHorloge.addEventListener('change', function(){
+      console.log("test");
+
+    });
+
+
+
+  }
+
+});
