867f7db2686805eea4b58be7c2e962cb68413983
NikoStage <niko>
2018-02-05 16:57:00 -0500

    Avec textures pour les jours et le choix du mois affecte le calendrier. Mais je dois enlever les écouteurs d'évènement pour le click car il se rajoute.

--- point/moisAvecPointer/components/jour-clickable.js
+++ point/moisAvecPointer/components/jour-clickable.js
@@ -8,70 +8,92 @@ AFRAME.registerComponent("jour-clickable", {
 
 			/* Bouton */
 			el = this.el,
+			leThis = this,
 
 			/* Permet d'aider pour l'écriture de la journée */
 			phraseDefaut = "Day Selected : ";
 
-			/* Lorsque qu'on hover le bouton */
-      el.addEventListener('hover-start', function() {
+			//sessionStorage.setItem("jourChoisi", JSON.stringify(new Array()));
+
+			/* Hover-start */
+			this.eventScalingBegining = function() {
 
 				el.setAttribute("scale","1.2 1.2 1.2");
 
-				el.addEventListener("click",calendrierAction);
+				el.addEventListener("click",leThis.calendrierAction.bind(leThis));
 
-      });
+      };
 
-			/* Lorsque qu'on ne hover plus le bouton */
-      el.addEventListener('hover-end', function() {
+			/* Hover-end */
+			this.eventScalingEnding = function() {
 
 				el.setAttribute("scale","1 1 1");
 
-				el.removeEventListener("click",calendrierAction);
+				el.removeEventListener("click",leThis.calendrierAction.bind(leThis));
 
-      });
+      };
 
-	  }
+			/* Lorsque qu'on hover le bouton */
+      el.addEventListener('hover-start', this.eventScalingBegining);
+
+			/* Lorsque qu'on ne hover plus le bouton */
+      el.addEventListener('hover-end', this.eventScalingEnding);
+
+	  },
+
+		remove:function() {
+
+			var el = this.el;
+
+			el.removeEventListener("hover-start",this.eventScalingBegining);
+			el.removeEventListener("hover-end",this.eventScalingEnding);
+
+		},
+
+		update:function() {
+
+			this.el.removeEventListener("click",this.calendrierAction.bind(this));
+
+		},
+
+		calendrierAction:function(evt) {
+
+			/* Référence à l'écran, le jour, la phrase de défaut et tableau de jours */
+			//sessionStorage.setItem("jourChoisi", JSON.stringify(new Array()));
+			var txtJour = document.querySelector("#textJour"),
+					el = evt.target,
+					phraseDefaut = "Day Selected : ",
+					jourChoisi = JSON.parse(sessionStorage.getItem('jourChoisi'));
+
+			/* Si on ne aucun jour choisi dernièrement*/
+			if(jourChoisi == null) {
+
+				jourChoisi = new Array();
+
+			}
+
+			/* Cherche notre jour dans le tableau, si rien est trouvé, on l'ajoute*/
+			if(jourChoisi.indexOf(el.getAttribute("data-jour")) == -1) {
+
+				/* Ajout du jour dans notre tableau */
+				jourChoisi.push(el.getAttribute("data-jour"));
+
+			}
+			/* Si le jour est déjà dans le tableau */
+			else if(jourChoisi.indexOf(el.getAttribute("data-jour")) != -1) {
+
+				var posJour = jourChoisi.indexOf(el.getAttribute("data-jour"));
+				jourChoisi.splice(posJour,1);
+
+			}
+			console.log(el);
+			/* Joue l'animation de fade-in/fade-out */
+			el.emit("playFade");
+
+			/* Enregistrement de la variable jourChoisi dans la sessionStorage */
+			sessionStorage.setItem("jourChoisi", JSON.stringify(jourChoisi));
+			txtJour.setAttribute("value", phraseDefaut + jourChoisi.toString());
+
+		}
 
 });
-
-/* Variables Globale */
-var jourChoisi = JSON.parse(sessionStorage.getItem(jourChoisi));
-
-/* Action d'ajouter la valeur au titre de jour */
-function calendrierAction(event) {
-
-	/* Référence à l'écran, le jour, la phrase de défaut */
-	var txtJour = document.querySelector("#textJour"),
-	el = event.target,
-	phraseDefaut = "Day Selected : ";
-
-	/* Si on ne aucun jour choisi dernièrement */
-	if(jourChoisi == null) {
-
-		jourChoisi = new Array();
-
-	}
-
-	/* Cherche notre jour dans le tableau, si rien est trouvé, on l'ajoute*/
-	if(jourChoisi.indexOf(el.getAttribute("data-jour")) == -1) {
-
-		/* Ajout du jour dans notre tableau */
-		jourChoisi.push(el.getAttribute("data-jour"));
-
-	}
-	/* Si le jour est déjà dans le tableau */
-	else if(jourChoisi.indexOf(el.getAttribute("data-jour")) != -1){
-
-		var posJour = jourChoisi.indexOf(el.getAttribute("data-jour"));
-		jourChoisi.splice(posJour,1);
-
-	}
-
-	/* Joue l'animation de fade-in/fade-out */
-	el.emit("playFade");
-
-	/* Enregistrement de la variable jourChoisi dans la sessionStorage */
-	sessionStorage.setItem("jourChoisi", JSON.stringify(jourChoisi));
-	txtJour.setAttribute("value", phraseDefaut + jourChoisi.toString());
-
-}
--- point/moisAvecPointer/components/lecture-cube.js
+++ point/moisAvecPointer/components/lecture-cube.js
@@ -18,12 +18,55 @@ AFRAME.registerComponent('lecture-cube', {
   lectureMois:function(evt) {
 
     var el = this.el,
-        leMoisChoisi = evt.detail.dropped.getAttribute('data-mois');
+        leMoisChoisi = evt.detail.dropped.getAttribute('data-mois'),
+        listeJours = document.querySelectorAll(".choixJour"),
+        txtJour = document.querySelector("#textJour");
 
     el.setAttribute("data-mois-choisi",leMoisChoisi);
     document.querySelector("#blocSousLecteur").setAttribute(
                            "material", "src:#" +
                             el.getAttribute("data-mois-choisi"));
+
+    /* Remet le texte par défaut */
+    txtJour.setAttribute("value", "Day Selected : ");
+    sessionStorage.setItem("jourChoisi", JSON.stringify(new Array()));
+
+    /* Si nous avons choisi un mois, on peut choisir la journée */
+    if(el.getAttribute("data-mois-choisi") != " ") {
+
+      /* Pour tout les jours */
+      for(var i = 0; i < listeJours.length; i++) {
+
+        listeJours[i].setAttribute("material","color:white");
+        listeJours[i].setAttribute("jour-clickable","");
+
+      }
+
+      /* Si c'est février on limite à 28 jours, cache le 29,30,31 */
+      if(el.getAttribute("data-mois-choisi") == "FEB") {
+
+        for(var j = 28; j < listeJours.length; j++) {
+
+          listeJours[j].removeAttribute("jour-clickable");
+          listeJours[j].setAttribute("material","color:red;");
+
+        }
+
+      }
+
+      /* Si c'est un mois avec 30 jours, cache le 31 */
+      else if(el.getAttribute("data-mois-choisi") == "NOV" ||
+              el.getAttribute("data-mois-choisi") == "APR" ||
+              el.getAttribute("data-mois-choisi") == "JUNE" ||
+              el.getAttribute("data-mois-choisi") == "SEPT") {
+
+           listeJours[30].removeAttribute("jour-clickable");
+           listeJours[30].setAttribute("material","color:red;");
+
+      }
+
+
+    }
 
   }
 
