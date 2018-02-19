cba0338162a616b29ded0756775aeccf56d66488
NikoStage <niko>
2018-02-19 17:32:22 -0500

    Début des lignes avec THREE.JS

--- test_CSV/js/lire-csv.js
+++ test_CSV/js/lire-csv.js
@@ -52,20 +52,22 @@ function deuxiemeFichier(){
                 lignesGraphe.setAttribute("id","leslignesGraphe");
                 var data = results.data;
                 //console.log(dataLife)
-                for(i=1;i<20;i++) {
 
-                  /* Crée le parent de la ligne pour le pays */
-                  ligneGraphe = document.createElement("a-entity");
-                  ligneGraphe.setAttribute("class","ligneGraphe");
-                  ligneGraphe.setAttribute("position",{x:-0.81,y:0,z:0.9});
-                  lignesGraphe.appendChild(ligneGraphe);
+                for(i=1;i<20;i++) {
 
                   var row = data[i];
                   var cells = row.join(",").split(",");
                   var rowLife = dataLife[i];
                   var cellsLife = rowLife.join(",").split(",");
 
-                  for(j=0;j<cells.length -1;j++) {
+                  /* Crée le parent de la ligne pour le pays */
+                  var ligneGraphe = document.createElement("a-entity");
+                  ligneGraphe.setAttribute("class","ligneGraphe");
+                  //ligneGraphe.setAttribute("position",{x:-0.81,y:0,z:0.9});
+                  ligneGraphe.object3D.position.set(-0.81,0,0.9);
+                  lignesGraphe.appendChild(ligneGraphe);
+
+                  for(j=0;j<cells.length - 1;j++) {
 
                     var fin = {x:-0.8, y:0.05, z:0.8};
                     var debut = {x:-0.8, y:0.05, z:0.8};
@@ -87,7 +89,7 @@ function deuxiemeFichier(){
 
                       //console.log(debut.y)
                       //console.log(fin.y)
-
+/*
                       ligneGraphe.setAttribute("line__" + j, {
 
                             start: debut,
@@ -96,7 +98,55 @@ function deuxiemeFichier(){
 
                       });
 
-                      ligneGraphe.setAttribute("visible","false");
+                      ligneGraphe.setAttribute("geometry", {
+                            primitive: "sphere",
+                            radius:0.01
+                            //depth:0.05,
+                            //height:0.05,
+                            //width:0.05,
+                      });
+
+                      ligneGraphe.setAttribute("material", {
+                            wireframe:true,
+                            color:"#0080ff",
+                            shader:"flat"
+                      });
+
+                      //ligneGraphe.object3D.position.set(debut.x,debut.y,debut.z);
+                      ligneGraphe.setAttribute("position",debut);
+
+                      //ligneGraphe.setAttribute("visible","false");*/
+
+                      //create a blue LineBasicMaterial
+                      var material = new THREE.LineBasicMaterial( { color: 0xFF0000 } );
+                      var geometry = new THREE.Geometry();
+
+                      /*var r = 800;
+              				for ( var i = 0; i < 100; i ++ ) {
+              					var x = debut.x;
+              					var y = debut.y;
+              					var z = debut.z;
+              					// positions
+              					positions.push( x, y, z );
+              					// colors
+              					colors.push( ( x / r ) + 0.5 );
+              					colors.push( ( y / r ) + 0.5 );
+              					colors.push( ( z / r ) + 0.5 );
+              				}*/
+
+
+                      //positions.push(debut.x,debut.y,debut.z);
+                      //geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ));
+                      //geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ));
+                      geometry.vertices.push(
+                      	new THREE.Vector3( debut.x, debut.y, debut.z ),
+                      	new THREE.Vector3( fin.x, fin.y, fin.z )
+                      );
+                      geometry.computeBoundingSphere();
+                      ligne = new THREE.Line(geometry,material)
+                      ligneGraphe.object3D.add(ligne);
+                      //console.log(ligneGraphe)
+
 
                     }
 
