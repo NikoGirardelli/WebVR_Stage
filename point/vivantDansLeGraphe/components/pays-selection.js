498a7184580da29fd5e4ef9d87df4e5ac8c007a0
NikoStage <niko>
2018-02-23 16:51:22 -0500

    Commencement de la scène. Fenêtre des choix des pays dans notre main gauche. La main de droite permet d'utiliser le laser.

---
+++ point/vivantDansLeGraphe/components/pays-selection.js
@@ -0,0 +1,141 @@
+/* S'occupe de savoir quel journée est saisie
+ * lorsque les boutons sont cliqué
+ */
+AFRAME.registerComponent("pays-selection", {
+
+		init: function () {
+
+			/* Référence à l'écran */
+      //var txtJour = document.querySelector("#textJour"),
+
+			/* Pays */
+			var el = this.el,
+					anim = document.createElement('a-animation'),
+					animText = document.createElement('a-animation');
+
+			switch(el.attributes[1].nodeValue) {
+
+				case "Americas":
+				anim.setAttribute("mixin","fadeAmericas");
+				break;
+
+				case "Africa":
+				anim.setAttribute("mixin","fadeAfrica");
+				break;
+
+				case "Europe":
+				anim.setAttribute("mixin","fadeEurope");
+				break;
+
+				case "Asia":
+				anim.setAttribute("mixin","fadeAsia");
+				break;
+
+			}
+			animText.setAttribute("mixin","fadeCouleur");
+			el.firstElementChild.appendChild(animText);
+			el.appendChild(anim);
+
+			/* Raycaster-intersected */
+			this.eventScalingBegining = function() {
+				console.log(el)
+				el.setAttribute("scale","1.2 1.2 1.2");
+
+      };
+
+			/* Raycaster-intersected-cleared */
+			this.eventScalingEnding = function() {
+
+				el.setAttribute("scale","1 1 1");
+
+      };
+
+			/* Ajoute ou supprime le pays */
+			this.calendrierAction = function() {
+
+				/*var paysChoisi = JSON.parse(sessionStorage.getItem('paysChoisi'));
+
+				/* Si on ne aucun pays choisi dernièrement
+				if(paysChoisi == null) {
+
+					paysChoisi = new Array();
+
+				}
+
+				// Cherche notre pays dans le tableau
+				if(paysChoisi.indexOf(el.getAttribute("data-pays")) == -1) {
+
+					/* Ajout du pays dans notre tableau
+					paysChoisi.push(el.getAttribute("data-pays"));
+
+				}
+				/* Si le pays est déjà dans le tableau
+				else if(paysChoisi.indexOf(el.getAttribute("data-pays")) != -1) {
+
+					/* Supprime le pays
+					var posPays = paysChoisi.indexOf(el.getAttribute("data-pays"));
+					paysChoisi.splice(posPays,1);
+
+				}*/
+
+				/* Joue l'animation de fade-in/fade-out */
+				switch(el.attributes[1].nodeValue) {
+					case "Americas":
+					el.emit("playFadeAmericas");
+					break;
+
+					case "Africa":
+					el.emit("playFadeAfrica");
+					break;
+
+					case "Europe":
+					el.emit("playFadeEurope");
+					break;
+
+					case "Asia":
+					el.emit("playFadeAsia");
+					break;
+				}
+
+				el.firstElementChild.emit("playFadeCouleur");
+				var maLigne = document.querySelector('[data-pays-ligne="'+el.getAttribute("data-pays")+'"]');
+				var visible = maLigne.getAttribute("visible");
+				maLigne.setAttribute("visible",!visible);
+
+				/* Enregistrement de la variable paysChoisi dans la sessionStorage
+				sessionStorage.setItem("paysChoisi", JSON.stringify(paysChoisi));*/
+
+			};
+
+	  },
+
+		play:function () {
+
+			/* Lorsqu'on clique le jour */
+			this.el.addEventListener("click",this.calendrierAction);
+
+			/* Lorsque qu'on hover le bouton */
+      this.el.addEventListener('raycaster-intersected', this.eventScalingBegining);
+
+			/* Lorsque qu'on ne hover plus le bouton */
+      this.el.addEventListener('raycaster-intersected-cleared', this.eventScalingEnding);
+
+		},
+
+		pause:function() {
+
+			this.el.removeEventListener("click",this.calendrierAction);
+			this.el.removeEventListener("raycaster-intersected",this.eventScalingBegining);
+			this.el.removeEventListener("raycaster-intersected-cleared",this.eventScalingEnding);
+
+		},
+
+		remove:function() {
+
+			this.el.removeEventListener("click",this.calendrierAction);
+			this.el.removeEventListener("raycaster-intersected",this.eventScalingBegining);
+			this.el.removeEventListener("raycaster-intersected-cleared",this.eventScalingEnding);
+
+		}
+
+});
